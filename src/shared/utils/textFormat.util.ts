4988663ff946ef7136981fc95a305b9f1062e4db
Amir <amirYuld@gmail.com>
2025-12-21 20:59:40 +0500

    refactor: simplify text formatting utility

--- src/shared/utils/textFormat.util.ts
+++ src/shared/utils/textFormat.util.ts
@@ -1,14 +1,11 @@
 import { INotification } from 'src/notification/notification.types'
 
 export function getFormattedText(notificationDto: INotification): string {
-	// Generate reminder times as plain text
 	const reminderTimes = notificationDto?.reminders
 		.map((time: Date) => {
 			return `- ${new Date(time).toLocaleString()}`
 		})
-		.join('\n') // Joining with newline characters for each reminder
-
-	// Return the formatted response without using unsupported <ul> or <li>
+		.join('\n')
 	return `✅ <b>${notificationDto?.message}</b>\n
 <b>${new Date(notificationDto?.date).toLocaleString()}</b>\n
 Напоминания:
